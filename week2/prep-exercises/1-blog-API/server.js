a574b74775990f7964d7a265ef5fd1af09b81dae
Fressia Barrios <fressiabarrios2@gmail.com>
2024-02-03 14:51:07 +0100

    added bonus exercise

--- week2/prep-exercises/1-blog-API/server.js
+++ week2/prep-exercises/1-blog-API/server.js
@@ -9,12 +9,14 @@ app.get('/', function (req, res) {
   res.send('<h1>Hello World</h1>');
 });
 
+// to create new blog
 app.post('/blogs', (req, res) => {
   const { title, content} = req.body; // accessing properties from req
   fs.writeFileSync(title, content);
   res.end('ok')
 })
 
+// to update existing blog
 app.put('/posts/:title', (req, res) => {
   const { title, content} = req.body;
   let fileExists = fs.existsSync(`./${req.params.title}`);
@@ -31,6 +33,7 @@ app.put('/posts/:title', (req, res) => {
   }
 })
 
+// to delete blog
 app.delete('/blogs/:title', (req, res) => {
   const title = req.params.title;
   let fileExists = fs.existsSync(`./${title}`);
@@ -43,6 +46,7 @@ app.delete('/blogs/:title', (req, res) => {
   }
 })
 
+// to read blog
 app.get('/blogs/:title', (req, res) => {
   const title = req.params.title;
   let fileExists = fs.existsSync(`./${title}`);
@@ -54,5 +58,19 @@ app.get('/blogs/:title', (req, res) => {
     res.end('This post does not exist!');
   }
 })
+
+// BONUS: to read all blogs
+app.get('/blogs', (req, res) => {
+  try {
+    const blogPosts = fs.readdirSync('./'); // synchronously reading contents in dir
+    const blogTitles = blogPosts
+      .filter((file) => fs.statSync(file).isFile() && file.includes(' ')) // filtering non-directory files
+      .map((file) => file.replace(/ /, '_')); // replace spaces with underscores
+    res.json(blogTitles); // sending only modified file names in the res
+  } catch (error) {
+    console.error('Error fetching blog titles');
+    res.status(500).send('Internal Server Error');
+  }
+});
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
