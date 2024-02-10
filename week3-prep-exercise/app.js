2198c390fbb6d3f16dae4fa88a8f6eff89ac7b47
Fressia Barrios <fressiabarrios2@gmail.com>
2024-02-10 21:43:46 +0100

    created register endpoint

--- week3-prep-exercise/app.js
+++ week3-prep-exercise/app.js
@@ -1,14 +1,15 @@
 import express from 'express';
-import { register, login, getProfile, logout } from './users.js';
+// import { register, login, getProfile, logout } from './users.js';
+import { register} from './users.js';
 
 let app = express();
 
 app.use(express.json());
 
-app.post ( "/register", register);
-app.post ( "/login",    login);
-app.post ( "/logout",   logout);
-app.get  ( "/profile",  getProfile);
+app.post ( '/register', register);
+// app.post ( '/login',    login);
+// app.post ( '/logout',   logout);
+// app.get  ( '/profile',  getProfile);
 
 
 app.listen(3000, () => {
--- week3-prep-exercise/users.js
+++ week3-prep-exercise/users.js
@@ -1,3 +1,42 @@
 import { v4 as generateUUID } from 'uuid';
 import { hash, compare } from 'bcrypt';
+import usersDatabase from './usersDatabase.json' assert { type: "json" };
 
+export const register = async (req, res) => {
+  // Check request body
+  const { username, password } = req.body;
+  if (!username || !password) {
+    res.status(400).json({ message: 'Please provide username and password' }).end();
+    return;
+  }
+
+  // Check if username already exists
+  const isUsernameExists = getUserByUsername(username) !== undefined;
+  if (isUsernameExists) {
+    res.status(400).json({ message: 'Username already exists' }).end();
+    return;
+  }
+
+  // Hash the password and create new user
+  const saltRounds = 10; 
+  const hashedPassword = await hash(password, saltRounds);
+  const newUser = {
+    id: generateUUID(),
+    username: username,
+    password: hashedPassword,
+  };
+
+  // Save user to usersDatabase
+  usersDatabase.push(newUser);
+
+  // Return success and the new user to the client
+  res.status(201).json({ 
+    id: newUser.id, 
+    username: newUser.username,
+  }).end();
+};
+
+// Helper function
+const getUserByUsername = (username) => {
+  return usersDatabase.find(user => user.username === username);
+};
