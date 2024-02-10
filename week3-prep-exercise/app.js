610198e9768384f66ee635748a438723a38fc1a4
Fressia Barrios <fressiabarrios2@gmail.com>
2024-02-10 23:14:03 +0100

    created login endpoint

--- week3-prep-exercise/app.js
+++ week3-prep-exercise/app.js
@@ -1,16 +1,15 @@
 import express from 'express';
 // import { register, login, getProfile, logout } from './users.js';
-import { register} from './users.js';
+import { register, login, profile} from './users.js';
 
 let app = express();
 
 app.use(express.json());
 
 app.post ( '/register', register);
-// app.post ( '/login',    login);
+app.post ( '/login',    login);
+app.get  ( '/profile',  profile);
 // app.post ( '/logout',   logout);
-// app.get  ( '/profile',  getProfile);
-
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
--- week3-prep-exercise/users.js
+++ week3-prep-exercise/users.js
@@ -1,10 +1,13 @@
 import { v4 as generateUUID } from 'uuid';
 import { hash, compare } from 'bcrypt';
+import fs from 'fs';
 import usersDatabase from './usersDatabase.json' assert { type: "json" };
+import jsonwebtoken from 'jsonwebtoken';
 
 export const register = async (req, res) => {
   // Check request body
   const { username, password } = req.body;
+
   if (!username || !password) {
     res.status(400).json({ message: 'Please provide username and password' }).end();
     return;
@@ -26,17 +29,53 @@ export const register = async (req, res) => {
     password: hashedPassword,
   };
 
-  // Save user to usersDatabase
-  usersDatabase.push(newUser);
-
-  // Return success and the new user to the client
-  res.status(201).json({ 
-    id: newUser.id, 
-    username: newUser.username,
-  }).end();
+  // Save user to usersDatabase and sync json file with new user
+  try {
+    usersDatabase.push(newUser);
+    fs.writeFileSync('./usersDatabase.json', JSON.stringify(usersDatabase, null, 2));
+    // Return success and the new user to the client
+    res.status(201).json({ 
+      id: newUser.id, 
+      username: newUser.username,
+    }).end();
+  } catch (error) {
+    res.status(500).json({ message: 'There was an error registering this user' }).end();
+  }
 };
 
-// Helper function
+export const login = async (req, res) => {
+   // Check request body
+   const { username, password } = req.body;
+
+   if (!username || !password) {
+    res.status(400).json({ message: 'Please provide username and password' }).end();
+    return;
+  }
+
+  // Find user in the database
+  const user = getUserByUsername(username);
+  if (!user) {
+    res.status(401).json({ message: 'Invalid username / password combination' }).end();
+    return;
+  }
+
+  // Check if password is correct by using bcrypt compare
+  const isPasswordCorrect = await compare(password, user.password);
+  if (!isPasswordCorrect) {
+    res.status(401).json({ message: 'Invalid username / password combination' }).end();
+    return;
+  }
+
+  // Login successfully - create a JW token
+  const SECRET = generateUUID();
+  const token = jsonwebtoken.sign(user.id, SECRET);
+
+  // Return the token to the client
+  res.status(200).json({ token }).end();
+};
+
+// Helper functions
+
 const getUserByUsername = (username) => {
   return usersDatabase.find(user => user.username === username);
 };
