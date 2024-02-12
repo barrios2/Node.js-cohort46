3908c3c934e4d79d0332cf646bc2477c3f7f9304
Fressia Barrios <fressiabarrios2@gmail.com>
2024-02-12 22:17:44 +0100

    installed test packages

--- HackYourTemperature/server.js
+++ HackYourTemperature/server.js
@@ -1,39 +1,5 @@
-import express from 'express';
-import fetch from 'node-fetch';
-import { keys } from './sources/keys.js';
+import app from "./app.js";
 
-const app = express();
 const PORT = 3000;
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.get('/', (req, res) => {
-  res.send('<h1>Hello from backend to frontend!</h1>');
-});
-
-app.post('/weather', async (req, res) => {
-  const { cityName } = req.body;
-
-  try {
-    if (!cityName) {
-      res.send('Please enter a city name');
-    } else {
-      const WEATHER_BASE_URL = 'https://api.openweathermap.org/data/2.5/weather';
-      const response = await fetch(`${WEATHER_BASE_URL}?units=metric&q=${cityName}&appid=${keys.API_KEY}`); // includes temp in celsius
-      
-      if (response.ok) {
-        const data = await response.json();
-        const { main: { temp } } = data;
-        res.send({ weatherText: `The temperature in ${cityName} is ${temp.toFixed()}°C` });
-      } else {
-        res.send({ weatherText: 'City is not found!' });
-      }
-    }
-  } catch (error) {
-    res.status(500).send({ error: 'Internal Server Error' });
-  }
-  res.end();
-});
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
